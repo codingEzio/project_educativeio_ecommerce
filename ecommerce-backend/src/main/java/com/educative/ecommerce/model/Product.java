1d421b76944e7f7b7483434d69ebd5f41fa93949
!Mensch <assassinste@gmail.com>
2022-10-17 10:31:54 +0800

    dev: Add entity 'Product' (DTO can be think of as an util)
    DTO (Data Transfer Object) could think of as an util for the Model; it
    is normally the object like JSON you send request (POST) to, it might
    have more or less fields than the fields in the Model you wrote.

---
+++ ecommerce-backend/src/main/java/com/educative/ecommerce/model/Product.java
@@ -0,0 +1,95 @@
+package com.educative.ecommerce.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private @NotNull String name;
+	private @NotNull String imageUrl;
+	private @NotNull double price;
+	private @NotNull String description;
+
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "category_id", nullable = false)
+	Category category;
+
+
+	public Product() {
+	}
+
+	public Product(String name, String imageUrl, double price, String description, Category category) {
+		super();
+
+		this.name = name;
+		this.imageUrl = imageUrl;
+		this.price = price;
+		this.description = description;
+		this.category = category;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+}
