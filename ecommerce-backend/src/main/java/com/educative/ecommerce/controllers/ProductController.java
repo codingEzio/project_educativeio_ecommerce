499c1184c2bf128fc46a82c1e56e4ebc4d73493e
!Mensch <assassinste@gmail.com>
2022-10-17 13:26:33 +0800

    feat: Implement Read,Update for Product (with category ID)

--- ecommerce-backend/src/main/java/com/educative/ecommerce/controllers/ProductController.java
+++ ecommerce-backend/src/main/java/com/educative/ecommerce/controllers/ProductController.java
@@ -4,16 +4,21 @@ package com.educative.ecommerce.controllers;
 import com.educative.ecommerce.common.ApiResponse;
 import com.educative.ecommerce.dto.product.ProductDto;
 import com.educative.ecommerce.model.Category;
+import com.educative.ecommerce.model.Product;
 import com.educative.ecommerce.service.CategoryService;
 import com.educative.ecommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -25,6 +30,14 @@ public class ProductController {
 
 	@Autowired
 	CategoryService cateService;
+
+	@GetMapping("/")
+	public ResponseEntity<List<ProductDto>> getProduct() {
+		List<ProductDto> productDtos = prodService.listProducts();
+
+		return new ResponseEntity<>(productDtos, HttpStatus.OK);
+	}
+
 
 	@PostMapping("/add")
 	public ResponseEntity<ApiResponse> addProduct(@RequestBody ProductDto productDto) {
@@ -43,6 +56,27 @@ public class ProductController {
 		return new ResponseEntity<>(
 				new ApiResponse(true, "Product has been added"),
 				HttpStatus.CREATED
+		);
+	}
+
+	@PostMapping("/update/{productID}")
+	public ResponseEntity<ApiResponse> updateProduct(@PathVariable("productID") Integer productID,
+	                                                 @RequestBody @Valid ProductDto productDto) {
+		Optional<Category> opCategory = cateService.readCategory(productDto.getCategoryId());
+
+		if (!opCategory.isPresent()) {
+			return new ResponseEntity<ApiResponse>(
+					new ApiResponse(false, "category is invalid"),
+					HttpStatus.CONFLICT
+			);
+		}
+
+		Category category = opCategory.get();
+		prodService.updateProduct(productID, productDto, category);
+
+		return new ResponseEntity<>(
+				new ApiResponse(true, "Product has been updated"),
+				HttpStatus.OK
 		);
 	}
 
--- ecommerce-backend/src/main/java/com/educative/ecommerce/service/ProductService.java
+++ ecommerce-backend/src/main/java/com/educative/ecommerce/service/ProductService.java
@@ -7,11 +7,25 @@ import com.educative.ecommerce.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class ProductService {
 
 	@Autowired
 	private ProductRepository productRepository;
+
+	public List<ProductDto> listProducts() {
+		List<Product> products = productRepository.findAll();
+		List<ProductDto> productDtos = new ArrayList<>();
+
+		for (Product product: products) {
+			productDtos.add(new ProductDto(product));
+		}
+
+		return productDtos;
+	}
 
 	public void addProduct(ProductDto productDto, Category category) {
 		Product product = getProductFromDto(productDto, category);
@@ -19,6 +33,13 @@ public class ProductService {
 		productRepository.save(product);
 	}
 
+	public void updateProduct(Integer productID, ProductDto productDto, Category category) {
+		Product product = getProductFromDto(productDto, category);
+
+		product.setId(productID);
+
+		productRepository.save(product);
+	}
 
 	public static Product getProductFromDto(ProductDto productDto, Category category) {
 		Product product = new Product();
@@ -27,7 +48,7 @@ public class ProductService {
 		product.setImageUrl(productDto.getImageUrl());
 		product.setPrice(productDto.getPrice());
 		product.setName(productDto.getName());
-		
+
 		product.setCategory(category);
 
 		return product;
